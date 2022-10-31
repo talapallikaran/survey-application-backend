3e70f5f48323a02a84501b3c6a54f62f982fb624
sonalratho26 <sonal.rathod@aimsinfosoft.com>
2022-10-31 18:19:22 +0530

    update create user api

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const userControllers = require("../controllers/users.controller");
+const upload = require('../middleware/upload');
+const Resize = require('../Resize');
 
 router.post("/login", userControllers.login, (req, res, next) => {
   res.send(req.data);
 });
 
-router.post("/register", userControllers.createUser, (req, res, next) => {
+router.post("/register", userControllers.createUser,  async (req, res, next) => {
   res.send(req.data);
 });
 
 router.get("/users", userControllers.listUser, (req, res, next) => {
+  res.send(req.data);
+});
+
+router.put("/users/:id", userControllers.updateUser, (req, res, next) => {
   res.send(req.data);
 });
 
