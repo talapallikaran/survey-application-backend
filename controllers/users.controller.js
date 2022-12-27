b9ab821a1457eabfc4f4d1bfbc860ddc82c31e17
@sonalratho26 <sonal.rathod@aimsinfosoft.com>
2022-12-27 17:46:48 +0530

    latest_updated_code

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const uuid = require("uuid");
-var User = require('./../models/user');
+var User = require("./../models/user");
 const uid = uuid.v4();
 const multer = require("multer");
+var Survey = require('./../models/survey');
 
 const listUser = function (req, res) {
   User.getUsers()
@@ -14,131 +15,349 @@ const listUser = function (req, res) {
     .catch(function (err) {
       return res.status(400).json({
         message: err,
-        statusCode: "400"
+        statusCode: "400",
       });
     });
-}
+};
 
 const createUser = (req, res, err) => {
-
-  if (!req.file) { 
+  if (!req.file) {
     return res.status(400).send({
-        message: "No file received or invalid file type",
-        success: false
+      message: "No file received or invalid file type",
+      success: false,
     });
-}
-  else {
-    User.isUserExists(req.body.email).then(isExists => {
+  } else {
+    User.isUserExists(req.body.email).then((isExists) => {
       if (isExists) {
         return res.status(400).json({
-          status: 'failed',
-          message: 'Email is taken.',
-          statusCode: "400"
+          status: "failed",
+          message: "Email is taken.",
+          statusCode: "400",
         });
-      }
-      else {
-
-        let image_src = (req.file) ? req.file.path : null;
+      } else {
+        let image_src = req.file ? req.file.path : null;
         console.log("image", image_src);
-        let { name, email, phone, password, confirmPassword, reporting_person_id } = req.body;
-        User.create({ name, email, phone, password, confirmPassword, reporting_person_id, image_src })
+        let {
+          name,
+          email,
+          phone,
+          role_id,
+          password,
+          confirmPassword,
+          reporting_person_id,
+          last_name,
+        } = req.body;
+
+        User.create({
+          name,
+          email,
+          phone,
+          role_id,
+          password,
+          confirmPassword,
+          reporting_person_id,
+          image_src,
+          last_name,
+        })
           .then(function (result) {
             console.log("result", result);
             return res.status(200).json({
-              status: 'success',
+              status: "success",
               statusCode: "200",
-              message: 'success! created account for new user'
+              message: "success! created account for new user",
             });
           })
           .catch(function (err) {
             return res.status(400).json({
               message: err,
-              statusCode: "400"
+              statusCode: "400",
             });
           });
       }
-
     });
   }
-
-
-}
+};
 
 const login = (request, response) => {
   const { email, password } = request.body;
-  User.isUserExists(email).then(isExists => {
-    if (!isExists) {
-      return response.status(401).json({
-        status: 'failed',
-        message: 'Invalid email or password!',
-        statusCode: "401"
+  let name;
+  let worker_uuid;
+  let getworker = [];
+  let workerdata = [];
+  User.isUserExists(email).then(
+    (isExists) => {
+      if (!isExists) {
+        return response.status(401).json({
+          status: "failed",
+          message: "Invalid email or password!",
+          statusCode: "401",
+        });
+      }
+      User.getUser(email, uuid).then((user) => {
+        bcrypt.compare(password, user.password, (error, isValid) => {
+          if (error) {
+            throw error;
+          }
+          if (!isValid) {
+            return response.status(401).json({
+              status: "failed",
+              message: "Invalid email or password??!",
+              accessToken: null,
+              statusCode: "401",
+            });
+          } else {
+            console.log("user", user);
+            //signing token with user id
+            const token = jwt.sign(
+              {
+                id: user.id,
+              },
+              process.env.API_SECRET,
+              {
+                expiresIn: 86400,
+              }
+            );
+            if (user.role_id === 2) {
+              User.getWorker(user.id).then(function (result) {
+                workerdata = result;
+                workerdata.map((data) => {
+                  name = data.worker;
+                  worker_uuid = data.wuuid;
+                  getworker.push({
+                    id: worker_uuid,
+                    name: name,
+                  });
+                });
+                response.status(200).json({
+                  status: "success",
+                  statusCode: "200",
+                  message: "Login Successfully!",
+                  accessToken: token,
+                  email: email,
+                  uuid: user.uuid,
+                  name: user.name,
+                  profile: user.image_src,
+                  role_id: user.role_id,
+                  worker_data: getworker,
+                });
+              });
+            } else {
+              console.log("worker");
+              response.status(200).json({
+                status: "success",
+                statusCode: "200",
+                message: "Login Successfully!",
+                accessToken: token,
+                email: email,
+                uuid: user.uuid,
+                name: user.name,
+                profile: user.image_src,
+                role_id: user.role_id,
+              });
+            }
+          }
+        });
+      });
+    },
+    (error) => {
+      response.status(400).json({
+        status: "failed",
+        statusCode: "400",
+        message: "Error while login.",
       });
     }
-    User.getUser(email, uuid).then(user => {
-      bcrypt.compare(password, user.password, (error, isValid) => {
-        if (error) {
-          throw error;
-        }
-        if (!isValid) {
-          return response.status(401).json({
-            status: 'failed',
-            message: 'Invalid email or password??!',
-            accessToken: null,
-            statusCode: "401"
-          });
-        }
-        //signing token with user id
-        const token = jwt.sign({
-          id: user.id
-        }, process.env.API_SECRET, {
-          expiresIn: 86400
-        });
-        response.status(200).json({
-          status: 'success',
-          statusCode: "200",
-          message: 'Login Successfully!',
-          accessToken: token,
-          email: email,
-          uuid: user.uuid,
-          name: user.name,
-          profile: user.image_src
-        });
-      });
-    });
-  }, error => {
-    response.status(400).json({
-      status: 'failed',
-      statusCode: "400",
-      message: 'Error while login.'
-    });
-  });
+  );
 };
 
 const updateUser = (req, res) => {
-  let image_src = (req.file) ? req.file.path : null;
+  let image_src = req.file ? req.file.path : req.body.image_src;
   console.log("image", image_src);
+  const { id } = req.params;
+  const { password } = req.body;
+  User.isUserExists(req.body.email).then((isExists) => {
+    console.log("isexistes", isExists);
+  });
+  User.getUserId(id).then((user) => {
+    if (password === user.password) {
+      User.updateUser({
+        id: req.params.id,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        role_id: req.body.role_id,
+        reporting_person_id: req.body.reporting_person_id,
+        image_src: image_src,
+        last_name: req.body.last_name,
+      })
+        .then(function (result) {
+          return res.status(200).json({
+            status: "success",
+            statusCode: "200",
+            message: "success! user data updated suucessfully",
+          });
+        })
+        .catch(function (err) {
+          return res.status(400).json({
+            message: err,
+          });
+        });
+    } else {
+      User.updateUserPassword({
+        id: req.params.id,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        role_id: req.body.role_id,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        reporting_person_id: req.body.reporting_person_id,
+        image_src: image_src,
+        last_name: req.body.last_name,
+      })
+        .then(function (result) {
+          console.log("result", result);
+          return res.status(200).json({
+            status: "success",
+            statusCode: "200",
+            message: "success! user data updated suucessfully",
+          });
+        })
+        .catch(function (err) {
+          return res.status(400).json({
+            message: err,
+          });
+        });
+    }
+  });
+};
 
-  User.updateUser({
-    id: req.params.id, name: req.body.name, email: req.body.email,
-    phone: req.body.phone, password: req.body.password, confirmPassword: req.body.confirmPassword, reporting_person_id: req.body.reporting_person_id, image_src: image_src
-  })
-    .then(function (result) {
-      return res.status(200).json({
-        status: 'success',
-        statusCode: "200",
-        message: 'success! user data updated suucessfully'
+const deleteUser = (request, response, error) => {
+  const { id } = request.params;
+  User.isUserExistsId(id).then((isExists) => {
+
+    if (!isExists) {
+      return response.status(401).json({
+        status: "failed",
+        message: "User not found",
+        statusCode: "401",
       });
-    })
-    .catch(function (err) {
-      return res.status(400).json({
-        message: err
+    } else {
+      User.getUserId(id).then((user) => {
+        if (user.role_id === 2) {
+          Survey.checkSubmissionExistsForDelete(user.id).then(async function (result) {
+            
+            if (result) {
+              User.DeleteReviews(user.id).then(function (result) {
+                if (result.length == 0) {
+                  User.DeleteSubmission(user.id).then(function (result) {
+    
+                    if (result.length === 0) {
+                      User.DeleteUser(user.id).then(function (result) {
+                        return response.status(200).json({
+                          status: "success",
+                          statusCode: "200",
+                          message: "success! user data deleted suucessfully",
+                        });
+                      });
+                    } else {
+                      return response.status(400).json({
+                        status: "error",
+                        statusCode: "400",
+                        message: error,
+                      });
+                    }
+                  });
+                }
+            })
+            }
+            else{
+         
+                User.DeleteSupervisaorReviewsSupervisor(user.id).then(function (result) {
+                
+                  if (result.length === 0) {
+                    User.DeleteSupervisaorSubmissionSuperviosr(user.id).then(function (result) {
+                    
+                      if (result.length === 0) {
+                        User.DeleteUser(user.id).then(function (result) {
+                          console.log("resultUser",result);
+                          return response.status(200).json({
+                            status: "success",
+                            statusCode: "200",
+                            message: "success! user data deleted suucessfully",
+                          });
+                        });
+                      } else {
+                        return response.status(400).json({
+                          status: "error",
+                          statusCode: "400",
+                          message: error,
+                        });
+                      }
+                    });
+                  }
+                });
+            }
+        })
+        } else {
+          Survey.checkSubmissionExistsForDelete(user.id).then(async function (result) {
+            if (result) {
+              User.DeleteReviews(user.id).then(function (result) {
+                if (result.length == 0) {
+                  User.DeleteSubmission(user.id).then(function (result) {
+                    if (result.length === 0) {
+                      User.DeleteUser(user.id).then(function (result) {
+                        return response.status(200).json({
+                          status: "success",
+                          statusCode: "200",
+                          message: "success! user data deleted suucessfully",
+                        });
+                      });
+                    } else {
+                      return response.status(400).json({
+                        status: "error",
+                        statusCode: "400",
+                        message: error,
+                      });
+                    }
+                  });
+                }
+              });
+            }
+            else {
+              User.DeleteSupervisaorReviews(user.id).then(function (result) {
+                if (result.length === 0) {
+                  User.DeleteSupervisaorSubmission(user.id).then(function (result) {
+                    if (result.length === 0) {
+                      User.DeleteUser(user.id).then(function (result) {
+                        return response.status(200).json({
+                          status: "success",
+                          statusCode: "200",
+                          message: "success! user data deleted suucessfully",
+                        });
+                      });
+                    } else {
+                      return response.status(400).json({
+                        status: "error",
+                        statusCode: "400",
+                        message: error,
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          })
+
+        }
       });
-    });
-}
+    }
+  });
+};
 
 module.exports = {
   login,
   createUser,
   listUser,
-  updateUser
-}
+  updateUser,
+  deleteUser,
+};
--- models/user.js
+++ models/user.js
@@ -2,14 +2,15 @@ const { pool } = require('../config');
 require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
-const uuid = require("uuid");
-const uid = uuid.v4();
+const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const multer = require("multer");
 const path = require("path");
 
+
 let isActive = 1;
 let deleteFlag = 0;
-let role_id = 3;
+// let role_id = 3;
 const date = new Date();
 
 // const storage = multer.diskStorage({
@@ -32,9 +33,11 @@ const date = new Date();
 
 const getUsers = function() {
     return new Promise(function(resolve, reject) {
-      pool.query('select u.*,r.name as reporting_person_name from users u left join users r on u.reporting_person_id=r.id ORDER by u.id asc', [])
+      pool.query('select u.*,r.name as reporting_person_name,s.name as role from users u left join users r on u.reporting_person_id=r.id left join roles s ON s.id = u.role_id ORDER by u.id asc', [])
         .then(function(results) {
+     
           resolve(results.rows);
+      
         })
         .catch(function(err) {
           reject(err);
@@ -46,7 +49,7 @@ const getUsers = function() {
     return new Promise(function(resolve, reject) {
       pool.query('SELECT * FROM users where id = $1', [id])
         .then(function(results) {
-          resolve(results.rows);
+          resolve(results.rows[0]);
         })
         .catch(function(err) {
           reject(err);
@@ -66,6 +69,18 @@ async function isUserExists(email) {
     });
 }
 
+async function isUserExistsId(id) {
+  return new Promise(resolve => {
+      pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+          if (error) {
+              throw error;
+          }
+
+          return resolve(results.rowCount > 0);
+      });
+  });
+}
+
 async function getUser(email, uuid) {
     return new Promise(resolve => {
         pool.query('SELECT * FROM users WHERE email = $1 or uuid = $2', [email, uuid], (error, results) => {
@@ -78,18 +93,35 @@ async function getUser(email, uuid) {
     });
 }
 
+async function getWorker(id) {
+  return new Promise(resolve => {
+      pool.query('select u.id,w.name as worker,w.uuid as wuuid from users u left join users w on w.reporting_person_id= u.id where u.id = $1 ORDER by u.id asc ', [id], (error, results) => {
+          if (error) {
+              throw error;
+          }
+
+          return resolve(results.rows);
+      });
+  });
+}
+
+
+
 
 async function create(data) {
-  console.log("data-------------------->",data);
+const uid = uuidv4();
     return new Promise(function(resolve, reject) {
-      validateUserData(data)
+      validateUserDatawithPassword(data)
         .then(function() {
+        
           return hashPassword(data.password);
         })
         .then(function(hash) {
-          return pool.query('INSERT INTO users (name, email,phone,role_id,isactive,deleteflag,password,uuid,updateddate,reporting_person_id,image_src) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9,$10,$11)', [data.name, data.email, data.phone, role_id, isActive, deleteFlag, hash, uid, date,data.reporting_person_id,data.image_src]);
+          return pool.query('INSERT INTO users (name, email,phone,role_id,isactive,deleteflag,password,uuid,updateddate,reporting_person_id,image_src,last_name) VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9,$10,$11,$12)', [data.name, data.email, data.phone, data.role_id, isActive, deleteFlag, hash, uid, date,data.reporting_person_id,data.image_src,data.last_name]);
         })
         .then(function(result) {
+          console.log("uuid----------------------------------",uid);
+   
           resolve(result.rows[0]);
         })
         .catch(function(err) {
@@ -98,6 +130,31 @@ async function create(data) {
     });
   }
 
+  async function updateUserPassword(data) {
+    return new Promise(function(resolve, reject) {
+      if (!data.id) {
+        reject('error: id missing')
+      }
+      else{
+        validateUserDatawithPassword(data)
+        .then(function() {
+          if(data.password)
+          return hashPassword(data.password);
+        })
+        .then(function(hash) {
+          return pool.query('UPDATE users SET name = $2,email = $3,phone = $4,role_id = $5,password = $6,reporting_person_id = $7,image_src = $8,last_name = $9 WHERE id = $1', [data.id, data.name, data.email, data.phone,data.role_id,hash,data.reporting_person_id,data.image_src,data.last_name]);
+          
+        })
+        .then(function(result) {
+          resolve(result.rows[0]);
+        })
+        .catch(function(err) {
+          reject(err);
+        });
+      }
+     
+    });
+  }
   async function updateUser(data) {
     return new Promise(function(resolve, reject) {
       if (!data.id) {
@@ -105,11 +162,9 @@ async function create(data) {
       }
       else{
         validateUserData(data)
-        .then(function() {
-          return hashPassword(data.password);
-        })
         .then(function(hash) {
-          return pool.query('UPDATE users SET name = $2,email = $3,phone = $4,password = $5,reporting_person_id = $6,image_src = $7 WHERE id = $1', [data.id, data.name, data.email, data.phone,hash,data.reporting_person_id,data.image_src]);
+          return pool.query('UPDATE users SET name = $2,email = $3,phone = $4,role_id = $5,reporting_person_id = $6,image_src = $7,last_name= $8 WHERE id = $1', [data.id, data.name, data.email, data.phone,data.role_id,data.reporting_person_id,data.image_src,data.last_name]);
+          
         })
         .then(function(result) {
           resolve(result.rows[0]);
@@ -123,6 +178,159 @@ async function create(data) {
   }
 
 
+  async function DeleteReviews(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('DELETE FROM reviews re USING  submissions sb  WHERE  re.submission_id = sb.id AND sb.user_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+
+  async function DeleteSupervisaorReviews(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('DELETE FROM supervisor_reviews bp USING supervisor_submissions sl WHERE bp.sv_submission_id = sl.id AND sl.worker_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+
+  
+  async function DeleteSupervisaorReviewsSupervisor(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('DELETE FROM supervisor_reviews bp USING supervisor_submissions sl WHERE bp.sv_submission_id = sl.id AND sl.user_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+  async function DeleteSubmission(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('delete from submissions where user_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+  async function DeleteSupervisaorSubmission(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('delete from supervisor_submissions where worker_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+
+  async function DeleteSupervisaorSubmissionSuperviosr(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('delete from supervisor_submissions where user_id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+
+  async function DeleteUser(id) {
+    return new Promise(function(resolve, reject) {
+      if (!id) {
+        reject('error: id missing')
+      }
+      else{
+       
+          pool.query('delete from users where id = $1', [id])
+            .then(function(results) {
+         
+              resolve(results.rows);
+          
+            })
+            .catch(function(err) {
+              reject(err);
+            });
+      }
+
+    });
+  }
+
+ 
 function hashPassword(password) {
     return new Promise(function(resolve, reject) {
       bcrypt.genSalt(10, function(err, salt) {
@@ -145,6 +353,26 @@ function hashPassword(password) {
   
   function validateUserData(data) {
     return new Promise(function(resolve, reject) {
+      if (!data.email || !data.phone) {
+        reject('email and Phone missing')
+      }
+      else {
+    
+        validateEmail(data.email)
+          .then(function(){
+            return validatePhone(data.phone);
+          })
+          .then(function() {
+            resolve();
+          })
+          .catch(function(err) {
+            reject(err);
+          });
+      }
+    });
+  }
+  function validateUserDatawithPassword(data) {
+    return new Promise(function(resolve, reject) {
       if (!data.password || !data.email || !data.phone) {
         reject('email and/or password, Phone missing')
       }
@@ -166,7 +394,6 @@ function hashPassword(password) {
       }
     });
   }
-  
   function validateEmail(email) {
     return new Promise(function(resolve, reject) {
       if (typeof (email) !== 'string') {
@@ -235,5 +462,15 @@ module.exports = {
     getUsers,
     findOneById,
     getUserId,
-    updateUser
+    updateUser,
+    updateUserPassword,
+    getWorker,
+    DeleteReviews,
+    DeleteUser,
+    DeleteSupervisaorReviews,
+    DeleteSubmission,
+    DeleteSupervisaorSubmission,
+    isUserExistsId,
+    DeleteSupervisaorSubmissionSuperviosr,
+    DeleteSupervisaorReviewsSupervisor
 }
